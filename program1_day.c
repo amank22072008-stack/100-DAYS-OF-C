8edccf0058ebce0ff016336d8e37a2603c2f16c4
amank22072008-stack <aman.k22072008@gmail.com>
2026-08-12 15:11:27 +0530

    Add files via upload

--- program1_day.c
+++ program1_day.c
@@ -26,3 +26,17 @@ int main() {
 
     return 0;
 }
+
+C:\TDM-GCC-64>cd ..
+
+C:\>cd C:\Users\Asus\OneDrive\Desktop\C FILES
+
+C:\Users\Asus\OneDrive\Desktop\C FILES>gcc program1_day.c -o program1_day.out
+
+C:\Users\Asus\OneDrive\Desktop\C FILES>program1_day.out
+10 2
+Sum=12, Diff=8, Product=20, Quotient=5
+C:\Users\Asus\OneDrive\Desktop\C FILES>program1_day.out
+7 3
+Sum=10, Diff=4, Product=21, Quotient=2
+C:\Users\Asus\OneDrive\Desktop\C FILES>

dbc0ee73f6bb43a19b7d75256fda652388129694
amank22072008-stack <aman.k22072008@gmail.com>
2026-08-19 18:09:32 +0530

    Add files via upload

---
+++ program20_day10.c
@@ -0,0 +1,62 @@
+/*Q20: Write a program to display the day of the week based on a number (1–7) using switch-case.
+
+
+Sample Test Cases:
+Input 1:
+1
+Output 1:
+Monday
+
+Input 2:
+5
+Output 2:
+Friday
+
+*/
+
+#include <stdio.h>
+
+int main() {
+    int day;
+    scanf("%d", &day);
+
+    switch(day) {
+        case 1:
+            printf("Monday");
+            break;
+        case 2:
+            printf("Tuesday");
+            break;
+        case 3:
+            printf("Wednesday");
+            break;
+        case 4:
+            printf("Thursday");
+            break;
+        case 5:
+            printf("Friday");
+            break;
+        case 6:
+            printf("Saturday");
+            break;
+        case 7:
+            printf("Sunday");
+            break;
+        default:
+            printf("Invalid day");
+    }
+
+    return 0;
+}
+
+C:\>cd C:\Users\Asus\OneDrive\Desktop\C FILES
+
+C:\Users\Asus\OneDrive\Desktop\C FILES>gcc program20_day10.c -o program20_day10.out
+
+C:\Users\Asus\OneDrive\Desktop\C FILES>program20_day10.out
+1
+Monday
+C:\Users\Asus\OneDrive\Desktop\C FILES>program20_day10.out
+5
+Friday
+C:\Users\Asus\OneDrive\Desktop\C FILES>

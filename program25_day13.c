d9397951f7a14998c8e40fb53c51bc554d5e5495
amank22072008-stack <aman.k22072008@gmail.com>
2026-08-22 22:53:30 +0530

    Add files via upload

---
+++ program25_day13.c
@@ -0,0 +1,71 @@
+/*Q25: Write a program to implement a basic calculator using switch-case for +, -, *, /, %.
+
+
+Sample Test Cases:
+Input 1:
+4 2 +
+Output 1:
+6
+
+Input 2:
+10 3 %
+Output 2:
+1
+
+Input 3:
+15 5 /
+Output 3:
+3
+
+*/
+
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    char op;
+
+    scanf("%d %d %c", &a, &b, &op);
+
+    switch (op) {
+        case '+':
+            printf("%d", a + b);
+            break;
+
+        case '-':
+            printf("%d", a - b);
+            break;
+
+        case '*':
+            printf("%d", a * b);
+            break;
+
+        case '/':
+            printf("%d", a / b);
+            break;
+
+        case '%':
+            printf("%d", a % b);
+            break;
+
+        default:
+            printf("Invalid Operator");
+    }
+
+    return 0;
+}
+
+C:\>cd C:\Users\Asus\OneDrive\Desktop\C FILES
+
+C:\Users\Asus\OneDrive\Desktop\C FILES>gcc program25_day13.c -o program25_day13.out
+
+C:\Users\Asus\OneDrive\Desktop\C FILES>program25_day13.out
+4 2 +
+6
+C:\Users\Asus\OneDrive\Desktop\C FILES>program25_day13.out
+10 3 %
+1
+C:\Users\Asus\OneDrive\Desktop\C FILES>program25_day13.out
+15 5 /
+3
+C:\Users\Asus\OneDrive\Desktop\C FILES>

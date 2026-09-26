a1fd8c298477a77c28a482939a1b767775028474
amank22072008-stack <aman.k22072008@gmail.com>
2026-09-26 20:04:46 +0530

    Add files via upload

---
+++ program95_day48.c
@@ -0,0 +1,56 @@
+/*Q95: Check if one string is a rotation of another.
+
+
+Sample Test Cases:
+Input 1:
+abcde
+deabc
+Output 1:
+Rotation
+
+Input 2:
+abc
+acb
+Output 2:
+Not rotation
+
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str1[100], str2[100], temp[200];
+
+    scanf("%s", str1);
+    scanf("%s", str2);
+
+    if (strlen(str1) != strlen(str2)) {
+        printf("Not rotation");
+    }
+    else {
+        strcpy(temp, str1);
+        strcat(temp, str1);
+
+        if (strstr(temp, str2) != NULL)
+            printf("Rotation");
+        else
+            printf("Not rotation");
+    }
+
+    return 0;
+}
+
+C:\>cd C:\Users\Asus\OneDrive\Desktop\C FILES
+
+C:\Users\Asus\OneDrive\Desktop\C FILES>gcc program95_day48.c -o program95_day48.out
+
+C:\Users\Asus\OneDrive\Desktop\C FILES>program95_day48.out
+abcde
+deabc
+Rotation
+C:\Users\Asus\OneDrive\Desktop\C FILES>program95_day48.out
+abc
+acb
+Not rotation
+C:\Users\Asus\OneDrive\Desktop\C FILES>
